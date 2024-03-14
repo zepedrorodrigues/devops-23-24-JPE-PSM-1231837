350936a75e03f24cf1783e0e35db98afa834832a
ozepedrorodrigues <1231837@isep.ipp.pt>
2024-03-14 12:00:50 +0000

    Fix #19

---
+++ CA2/Part1/gradle_demo/src/test/java/basic_demo/AppTest.java
@@ -0,0 +1,12 @@
+package basic_demo;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class AppTest {
+    @Test public void testAppHasAGreeting() {
+        App classUnderTest = new App();
+        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    }
+
+}
