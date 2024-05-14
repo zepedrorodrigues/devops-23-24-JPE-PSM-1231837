12a5346dce06280fe56ec396e921d0be51ec99a3
ozepedrorodrigues <1231837@isep.ipp.pt>
2024-05-14 22:16:01 +0100

    [fix] #31 - corrected build.gradle, app.js and ServletInitializer in order for app to run

--- CA2/Part2/demo/main/js/app.js
+++ CA2/Part2/demo/main/js/app.js
@@ -15,7 +15,8 @@ class App extends React.Component { // <1>
 	}
 
 	componentDidMount() { // <2>
-		client({method: 'GET', path: '/basic-0.0.1-SNAPSHOT/api/employees'}).done(response => {			this.setState({employees: response.entity._embedded.employees});
+		client({method: 'GET', path: '/basic-0.0.1-SNAPSHOT/api/employees'}).done(response => {
+			this.setState({employees: response.entity._embedded.employees});
 		});
 	}
 
