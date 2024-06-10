92e827e8c7e1b690cf5f42778d39d6eb506a650a
ozepedrorodrigues <1231837@isep.ipp.pt>
2024-06-10 17:11:46 +0100

    [docs] #38 - Minor corrections to README.md

--- CA2/Part2/demo/webpack.config.js
+++ CA2/Part2/demo/webpack.config.js
@@ -1,25 +1,25 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = {
     entry: './src/main/js/app.js',
-    devtool: 'sourcemaps',
+    devtool: 'source-map',  // Correct devtool configuration
     cache: true,
     mode: 'development',
     output: {
-        path: __dirname,
-        filename: './src/main/resources/static/built/bundle.js'
+        path: path.resolve(__dirname, 'src/main/resources/static/built'),
+        filename: 'bundle.js'
     },
     module: {
         rules: [
             {
-                test: path.join(__dirname, '.'),
-                exclude: /(node_modules)/,
-                use: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                        presets: ['@babel/preset-env', '@babel/preset-react']
                     }
-                }]
+                }
             }
         ]
     }
